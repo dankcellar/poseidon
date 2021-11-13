49ffc76afbe9786dba54f333447745c2e23f900c
Atik <atiklabs@pm.me>
2021-11-14 00:08:52 +0100

    Autorole separated by fish, shark, whale seems a mandatory thing to do.

--- client/src/pages/Verify.js
+++ client/src/pages/Verify.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import {useWeb3React} from "@web3-react/core";
 import {verifySignature} from "../utils/api";
 import {injected} from "../utils/connectors";
+import {addErrorMessage, addMessage} from "../utils/messages";
 
 export default function Verify(props) {
+    const [verified, setVerified] = useState(false);
     const discordId = props.match.params.id;
 
     const {active, activate, library, account} = useWeb3React();
@@ -18,8 +20,14 @@ export default function Verify(props) {
 
     const signMessage = async () => {
         if (!!library) {
-            const signature = await library.eth.personal.sign(discordId, account);
-            await verifySignature(discordId, signature, account);
+            try {
+                const signature = await library.eth.personal.sign(discordId, account);
+                await verifySignature(discordId, account, signature);
+                setVerified(true);
+                addMessage("Verified successfully");
+            } catch (e) {
+                addErrorMessage(e.message);
+            }
         }
     };
 
@@ -29,6 +37,21 @@ export default function Verify(props) {
                 <div className="verify-not-active">
                     <p>To verify your Discord account, please connect Metamask.</p>
                     <button onClick={connect}>Connect</button>
+                </div>
+            );
+        }
+        if (discordId.length < 10) {
+            return (
+                <div className="verify-not-active">
+                    <p>Discord account is invalid, please contact an admin in Discord.</p>
+                </div>
+            );
+        }
+        if (verified) {
+            return (
+                <div className="verify-success">
+                    <p>Verified successfully, please let some time for the machines to get you verified.</p>
+                    <p>Typically it takes between minutes and couple of hours, if after that time you are not verified contact an Admin in Discord.</p>
                 </div>
             );
         }
--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,6 +2,7 @@ import http from "./httpservice";
 
 export async function verifySignature(discordId, account, signature) {
     const r = await http.get(process.env.REACT_APP_API_VERIFY + discordId + "/" + account + "/" + signature);
-    if (r.data.error) throw r.data.error;
-    return r.data;
+    if (r.status !== 200) {
+        throw new Error("Cannot verify at this moment. Try again later.");
+    }
 }
