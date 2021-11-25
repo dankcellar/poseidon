f91ea6f30aa18827bb308f45e63e648d8f8ffbba
Atik <atiklabs@pm.me>
2021-11-25 18:48:15 +0100

    Update with bots removed.

--- client/src/utils/api.js
+++
@@ -1,8 +0,0 @@
-import http from "./httpservice";
-
-export async function verifySignature(discordId, account, signature) {
-    const r = await http.get(process.env.REACT_APP_API_VERIFY + discordId + "/" + account + "/" + signature);
-    if (r.status !== 200) {
-        throw new Error("Cannot verify at this moment. Try again later.");
-    }
-}
--- generator/contract.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "name": "Poseidon",
-    "description": "",
-    "image": "",
-    "external_link": "",
-    "seller_fee_basis_points": 250,
-    "fee_recipient": "0x..."
-};
